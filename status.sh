e127d9937cb4d937816a646e93aa3b913dc25ae6
MonPham <demondhunter93@gmail.com>
2023-02-19 20:48:16 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,5 +2,5 @@
 
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   echo "${docker_name} :"
-  docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning:'
+  docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning\|earnings'
 done

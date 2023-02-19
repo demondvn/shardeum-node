aa44980d0651487fa4ad9ea504648bce46cd5d58
MonPham <demondhunter93@gmail.com>
2023-02-19 14:52:21 +0700

    Create status.sh

---
+++ status.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
+  echo "${docker_name} :"
+  docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning:'
+done

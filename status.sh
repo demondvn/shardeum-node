61afff036ded65c17f8c7916599abd00bc08aac2
MonPham <demondhunter93@gmail.com>
2023-02-20 13:09:23 +0700

    .

--- status.sh
+++ status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
-  echo "${docker_name} :"
-  docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning\|earnings'
+  echo "########## ${docker_name} #########"
+  docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning\|earnings\|lockedStake:'
+  echo ""
 done

19d64a91c70b0ed4aba3032c1b464c0d3422a4ab
MonPham <demondhunter93@gmail.com>
2023-02-20 11:19:00 +0700

    Update docker-up.sh

--- docker-up.sh
+++ docker-up.sh
@@ -17,6 +17,7 @@
 # }
 # # docker-compose-safe -f docker-compose.yml up -d --scale "shardeum-dashboard=${SHARDEUM_INSTANCE}"
 read -p "How many instance you want (max 100): " SHARDEUM_INSTANCE
+read -p "Password for Dash: " DASHPASS
 DASHPORT=8080
 SHMEXT=9001
 SHMINT=10001
@@ -24,7 +25,7 @@ for index in $(seq 0 $((SHARDEUM_INSTANCE-1))); do
   echo "$((DASHPORT + index))"
   echo "$((SHMEXT + index))"
   echo "$((SHMINT + index))"
-  docker run -d --rm --name shardeum-node-$(index) -e SHMEXT=$((SHMEXT + index)) -e DASHPASS=P@ssw0rd -e SHMINT=$((SHMINT + index)) -e DASHPORT=$((DASHPORT + index)) \
+  docker run -d --rm --name shardeum-node-$(index) -e SHMEXT=$((SHMEXT + index)) -e DASHPASS=$(DASHPASS) -e SHMINT=$((SHMINT + index)) -e DASHPORT=$((DASHPORT + index)) \
   -p $((SHMEXT + index)):$((SHMEXT + index)) -p $((SHMINT + index)):$((SHMINT + index)) -p $((DASHPORT + index)):$((DASHPORT + index)) \
   -e APP_SEEDLIST="archiver-sphinx.shardeum.org" -e APP_MONITOR="monitor-sphinx.shardeum.org" -e APP_IP=auto \
   -e SERVERIP=$(curl https://ipinfo.io/ip) test-dashboard || return

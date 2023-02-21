f662cf0655b8ef55b40da7184218c36bc1bd1969
MonPham <demondhunter93@gmail.com>
2023-02-21 16:18:48 +0700

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -3,7 +3,7 @@
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   state=$(docker exec "${docker_name}" operator-cli status | grep 'state' | awk '{print $2}')
   echo "$docker_name : $state"
-  if [ "$state" == "stopped" ]; then
+  if [[ "$state" == "stopped" ]]; then
     echo "START SHARDEUM"
     docker exec "${docker_name}" operator-cli start
   fi

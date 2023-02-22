cc413ff2b706c9dff65db58a1bfdb9cc08759564
MonPham <demondhunter93@gmail.com>
2023-02-22 14:06:54 +0700

    .

--- shell.sh
+++ shell.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-
-docker-safe() {
-  if ! command -v docker &>/dev/null; then
-    echo "docker is not installed on this machine"
-    exit 1
-  fi
-
-  if ! docker $@; then
-    echo "Trying again with sudo..."
-    sudo docker $@
-  fi
-}
-
-docker-safe exec -it shardeum-dashboard /bin/bash
+NODE = $1
+if [[!$NODE]]then
+  read -p "Insert node number: "NODE
+if
+docker exec -it shardeum-dashboard-$NODE /bin/bash

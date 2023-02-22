7760617cf1d582efebf81d822b5451ad9044e4c8
MonPham <demondhunter93@gmail.com>
2023-02-22 11:13:01 +0700

    .

--- restore.sh
+++ restore.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #!/bin/bash
 
-for file in $(ls shardeum-node-*.json | sed 's/\.json$//'); do
+for file in $(ls backup/shardeum-node-*.json | sed 's/\.json$//'); do
   echo "$(pwd)/$file.json"
   docker cp "$(pwd)/$file.json" $(file):/node/cli/build/secrets.json
 

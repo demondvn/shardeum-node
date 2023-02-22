4f3782d69d2bd863838b2b7e4f4bcfb4d3f5342e
MonPham <demondhunter93@gmail.com>
2023-02-22 11:09:20 +0700

    .

--- backup.sh
+++ backup.sh
@@ -3,3 +3,5 @@ mkdir backup
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   docker exec "${docker_name}" cat cli/build/secrets.json >>  backup/"${docker_name}.json"
 done
+echo "Backup done ! check your folder backup"
+ls backup/
--- restore.sh
+++ restore.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+#!/bin/bash
+
+for file in $(ls shardeum-node-*.json | sed 's/\.json$//'); do
+  echo "$(pwd)/$file.json"
+  docker cp "$(pwd)/$file.json" $(file):/node/cli/build/secrets.json
+
+done
+echo "Done"

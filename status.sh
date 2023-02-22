f3189d2810bb8af3b7ad86cc364ddbea20709251
MonPham <demondhunter93@gmail.com>
2023-02-22 13:49:42 +0700

    .

--- status.sh
+++ status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
-  echo "########## ${docker_name} ######### version: $(docker exec "${docker_name}" cat validator/package.json | grep version)"
+  echo "########## ${docker_name} ######### $(docker exec "${docker_name}" cat validator/package.json | grep version)"
 
   docker exec "${docker_name}" operator-cli status | grep 'state\|totalTimeRunning\|earnings\|lockedStake\|nominatorAddress\|nomineeAddress'
   echo ""

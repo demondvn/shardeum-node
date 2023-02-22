102648338f2779fa4d1eab471bdf9667493caee1
MonPham <demondhunter93@gmail.com>
2023-02-22 17:08:47 +0700

    .

--- cleanup.sh
+++ cleanup.sh
@@ -5,3 +5,4 @@ docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name
   docker rm -f "${docker_name}" 
   echo "remove ${docker_name}"
 done
+docker image rm -f test-dashboard

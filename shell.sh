fa34c73a897217d14c8fe92da37c4d5252256f3d
MonPham <demondhunter93@gmail.com>
2023-02-22 14:07:19 +0700

    .

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 NODE = $1
-if [[!$NODE]]then
+if [[!"$NODE"]]then
   read -p "Insert node number: "NODE
 if
-docker exec -it shardeum-dashboard-$NODE /bin/bash
+docker exec -it "shardeum-dashboard-$NODE" /bin/bash

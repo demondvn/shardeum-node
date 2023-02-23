db2a224dba50a7cd4a0feb52ef2a3ef46ba1e6cb
MonPham <demondhunter93@gmail.com>
2023-02-23 22:14:22 +0700

    udpate remove duplicate

--- removeduplicate.sh
+++ removeduplicate.sh
@@ -1,3 +1,10 @@
+
 #!/bin/bash
 cd backup
-jq -s 'unique' input.json > output.json
+for file in $(ls shardeum-node-*.json | sed 's/\.json$//'); do
+    echo "$file.json"
+    jq -s 'unique' "$file.json" > "$file_output.json"
+    echo "Removed $(($(wc -l < "$file.json") - $(wc -l < "$file_output.json"))) duplicate entries."
+    jq '.[0]' "$file_output.json" > $file.json
+done
+echo "Done"

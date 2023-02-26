be1b6ea2dc765ebfa140fa36ba5382d1a61890b7
MonPham <demondhunter93@gmail.com>
2023-02-27 02:43:03 +0700

    .

--- start_all.sh
+++ start_all.sh
@@ -5,8 +5,8 @@ then
     sudo apt-get install screen
 fi
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
-  screen -dmS "${docker_name}" bash -c " ./_start.sh ${docker_name} > start.log"
+  screen -dmS "${docker_name}" bash -c " ./_start.sh ${docker_name} >> start.log"
   echo "${docker_name}"
 done
-sleep 5
+sleep 10
 tail start.log 

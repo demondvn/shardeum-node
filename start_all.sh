4dd2dc5b208bf3bf9ccd552bef8f3da127772ef9
MonPham <demondhunter93@gmail.com>
2023-02-28 11:59:43 +0700

    .

--- start_all.sh
+++ start_all.sh
@@ -7,6 +7,6 @@ fi
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   screen -dmS "${docker_name}" bash -c " ./_start.sh ${docker_name} >> start.log"
 done
-# echo "\`cat start.log\` to more detail"
+echo "\`cat start.log\` to more detail"
 sleep 30
 tail start.log 

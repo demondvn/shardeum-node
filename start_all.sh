577c2e2e0d2476b179f7e558dbdd09b1393d0828
MonPham <demondhunter93@gmail.com>
2023-02-28 11:56:40 +0700

    .

--- start_all.sh
+++ start_all.sh
@@ -2,10 +2,11 @@
 if ! command -v screen &> /dev/null
 then
     echo "screen is not installed. Installing..."
-    sudo apt-get install screen
+    sudo apt-get install screen -y
 fi
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   screen -dmS "${docker_name}" bash -c " ./_start.sh ${docker_name} >> start.log"
 done
-sleep 15
+# echo "\`cat start.log\` to more detail"
+sleep 30
 tail start.log 
--- status.sh
+++ status.sh
@@ -7,7 +7,7 @@ fi
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${current_time}" > log
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
-    screen -dmS "${docker_name}" bash -c " ./_status.sh ${docker_name} | tr '\n' ' ' >> log && echo '' >> log"
+    screen -dmS "${docker_name}" bash -c " ./_status.sh ${docker_name} >> log"
 done
 #sleep 5
 tail -f log

897444fe1ec4572d6723e3e82fca55049266c212
MonPham <demondhunter93@gmail.com>
2023-02-28 11:54:13 +0700

    Update _status.sh

--- _status.sh
+++ _status.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo $1 
-docker exec "$1" cat validator/package.json | grep version
-docker exec "$1" operator-cli status | grep "state\|totalTimeRunning\|nominatorAddress\|nomineeAddress"
+echo "######## $1 $(docker exec "$1" cat validator/package.json | grep version) ########"
+docker exec "$1" operator-cli status | grep "state\|totalTimeRunning\|earnings:\|nominatorAddress\|nomineeAddress"

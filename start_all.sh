087f20ff9fa31a072748cf47bbb29f80c3fb7f89
MonPham <demondhunter93@gmail.com>
2023-02-28 12:01:49 +0700

    .

--- start_all.sh
+++ start_all.sh
@@ -4,6 +4,8 @@ then
     echo "screen is not installed. Installing..."
     sudo apt-get install screen -y
 fi
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "${current_time}" > log
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
   screen -dmS "${docker_name}" bash -c " ./_start.sh ${docker_name} >> start.log"
 done

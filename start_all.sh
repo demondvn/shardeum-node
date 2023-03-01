f1c542a59505617d98317ffbbb17d995309676ae
MonPham <demondhunter93@gmail.com>
2023-03-01 08:40:38 +0700

    .

--- start_all.sh
+++ start_all.sh
@@ -1,9 +1,13 @@
-@@ -6,6 +6,8 @@ docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name
+#!/bin/bash
+
+docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
+  state=$(docker exec "${docker_name}" operator-cli status | grep 'state' | awk '{print $2}')
+  echo "$docker_name : $state"
   if [[ "$state" == "stopped" ]]; then
     echo "START SHARDEUM"
     docker exec "${docker_name}" operator-cli start
     docker exec "${docker_name}" sh -c 'operator-cli set external_port $SHMEXT'
     docker exec "${docker_name}" sh -c 'operator-cli set internal_port $SHMINT'
   fi
-
+  
 done

1992374b9ca69d3cb2391421f725482f581a33f4
MonPham <demondhunter93@gmail.com>
2023-03-03 22:46:13 +0700

    update status

--- status.sh
+++ status.sh
@@ -6,6 +6,12 @@ then
 fi
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${current_time}" > log
+trap out SIGINT
+function out(){
+echo -e "\nActive Node: $(cat log | grep -c 'active')"
+echo -e "Standby Node: $(cat log | grep -c 'standby')"
+exit 1
+}
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
     screen -dmS "${docker_name}" bash -c " ./_status.sh ${docker_name} >> log"
 done

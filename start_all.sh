654f614bd21ee0910f5b79a05569bbb1d27e1c5f
MonPham <demondhunter93@gmail.com>
2023-03-09 16:17:49 +0700

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
-  state=$(docker exec "${docker_name}" operator-cli status | grep 'state' | awk '{print $2}')
-  echo "$docker_name : $state"
-  if [[ "$state" == "stopped" ]]; then
+  port=$(docker exec "${docker_name}" operator-cli status | grep 'checkPort' | awk '{print $2}')
+  echo "$docker_name port open: $port"
+  if [[ "$port" == "false" ]]; then
     echo "START SHARDEUM"
     docker exec "${docker_name}" operator-cli start
     docker exec "${docker_name}" sh -c 'operator-cli set external_port $SHMEXT'

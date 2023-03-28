4dffc0c0eca9609099cc6257d80d83202b598c2d
MonPham <demondhunter93@gmail.com>
2023-03-28 17:03:18 +0700

    .

--- _start.sh
+++ _start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker_name=$1
-state=$(docker exec "${docker_name}" operator-cli status | grep 'state' | awk '{print $2}')
-  echo "$docker_name : $state"
-  if [[ "$state" == "stopped" ]]; then
-    echo "START SHARDEUM"
-    docker exec "${docker_name}" operator-cli start
-    docker exec "${docker_name}" sh -c 'operator-cli set external_port $SHMEXT'
-    docker exec "${docker_name}" sh -c 'operator-cli set internal_port $SHMINT'
-  fi
+
+# Get the names of all running Docker containers that start with "shardeum-node"
+docker ps --format '{{.Names}}' | grep '^shardeum-node' | while read docker_name; do
+        echo "Check $docker_name"
+  docker exec $docker_name operator-cli start
+  # Send a curl command to the container to load a file
+  
+echo ""
+done
